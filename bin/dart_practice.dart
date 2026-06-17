534ab8fa4635658b8fdab28a61e66f69cf465e0f
HASANALBANNA2024 <allbannamdhasan48@gmail.com>
2026-06-17 12:35:34 +0600

    git branching practice

---
+++ bin/dart_practice.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+void main() {
+  print('--- Simple Dart CLI Calculator ---');
+
+  // Taking first number as input
+  stdout.write('Enter first number: ');
+  double? num1 = double.tryParse(stdin.readLineSync()!);
+
+  // Taking operator as input
+  stdout.write('Enter operator (+, -, *, /): ');
+  String? operator = stdin.readLineSync();
+
+  // Taking second number as input
+  stdout.write('Enter second number: ');
+  double? num2 = double.tryParse(stdin.readLineSync()!);
+
+  // Validation to ensure numbers were entered
+  if (num1 == null || num2 == null) {
+    print('Error: Please enter valid numbers.');
+    return;
+  }
+
+  // Performing calculation based on operator
+  switch (operator) {
+    case '+':
+      print('Result: ${num1 + num2}');
+      break;
+    case '-':
+      print('Result: ${num1 - num2}');
+      break;
+    case '*':
+      print('Result: ${num1 * num2}');
+      break;
+    case '/':
+      if (num2 != 0) {
+        print('Result: ${num1 / num2}');
+      } else {
+        print('Error: Division by zero is not allowed.');
+      }
+      break;
+    default:
+      print('Error: Invalid operator.');
+  }
+}
